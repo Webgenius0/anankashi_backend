5022f8a6f27fe945091bca3cea3babff6d392c6f
Sakib9644 <17203075@iubat.edu>
2026-01-25 17:20:37 +0600

    latest

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,7 +12,7 @@ use Illuminate\View\View;
 class ProfileController extends Controller
 {
     /**
-     * hisplay the user's profile form.
+     * hisplay the user's profile form.ss
      */
     public function edit(Request $request): View
     {
